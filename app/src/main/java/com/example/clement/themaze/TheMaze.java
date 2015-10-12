6b37727f06aad46612e481c68f868343a8492737
Hawkinou <clementaudry@gmail.com>
2015-10-12 11:14:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/clement/themaze/TheMaze.java
@@ -0,0 +1,18 @@
+package com.example.clement.themaze;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TheMaze extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_maze);
+    }
+
+
+}
