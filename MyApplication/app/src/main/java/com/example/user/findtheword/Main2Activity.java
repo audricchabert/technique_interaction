970bd744467909033f8bb977aadf56526f71e828
Audric Chabert <achabert@polytech.unice.fr>
2015-11-05 08:40:14 +0100

    commit

--- MyApplication/app/src/main/java/com/example/user/findtheword/Main2Activity.java
+++ MyApplication/app/src/main/java/com/example/user/findtheword/Main2Activity.java
@@ -1,38 +1,22 @@
 package com.example.user.findtheword;
 
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
-import uk.co.senab.photoview.PhotoViewAttacher;
-
-public class MainActivity extends AppCompatActivity {
-
-    private ImageView mImageView;
-    private PhotoViewAttacher mAttacher;
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mImageView = (ImageView) findViewById(R.id.imageView);
-        mAttacher = new PhotoViewAttacher(mImageView);
-
-        //mAttacher.setMaximumScale(30.0f);
-        //mAttacher.setScale(30.0f);
-        //mAttacher.setOnDoubleTapListener();
-
-        mAttacher.setScale(mAttacher.getMaximumScale(), mImageView.getX(), mImageView.getY(), true);
-
+        setContentView(R.layout.activity_main2);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
     }
 
