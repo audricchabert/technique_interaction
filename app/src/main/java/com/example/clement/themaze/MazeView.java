e335c50861fa9ba388d075964ded9da6c2323aa8
Hawkinou <clementaudry@gmail.com>
2015-11-07 16:51:26 +0100

    swag

--- app/src/main/java/com/example/clement/themaze/MazeView.java
+++ app/src/main/java/com/example/clement/themaze/MazeView.java
@@ -2,20 +2,13 @@ package com.example.clement.themaze;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Picture;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.R;
-import java.io.File;
-import java.io.InputStream;
-import java.net.URL;
 
 public class MazeView extends SurfaceView {
     Paint paint;
@@ -68,26 +61,20 @@ public class MazeView extends SurfaceView {
     }
     @Override
     protected void onMeasure(int w, int h){
-        setMeasuredDimension(w, h);
+        int size =Math.min( w,h);
+
+        setMeasuredDimension(size, size);
         this.w=getMeasuredWidth();
         this.h=getMeasuredHeight();
+
     }
 
     @Override
     public void onDraw(Canvas canvas){
-<<<<<<< HEAD
-=======
-        /*try ( InputStream is = new URL( file_url ).openStream() ) {
-            Bitmap bitmap = BitmapFactory.decodeStream( is );
-        }*/
-        InputStream resource = getResources().openRawResource(R.drawable.background_maze);
-        Bitmap bitmap = BitmapFactory.decodeStream(resource);
 
-        //canvas.drawBitmap(background, 15,15,this.paint);
->>>>>>> origin/master
+        Drawable d = getResources().getDrawable(R.drawable.chemin);
         if (mapView) {
             int largeurCase =Math.min( w / 16,h/16);
-            //paint.setStyle(Paint.Style.FILL_AND_STROKE);
             paint.setColor(Color.BLACK);
 
             for (int i = 0; i < maze.getX(); i++) {
@@ -108,6 +95,10 @@ public class MazeView extends SurfaceView {
                     else if (maze.getGrille()[i][j] == 3) {
                         paint.setColor(Color.RED);
                         canvas.drawRect(largeurCase*j,largeurCase*i,largeurCase*(j+1),largeurCase*(i+1),paint);
+                    }
+                    else{
+                        d.setBounds(largeurCase*j,largeurCase*i,largeurCase*(j+1),largeurCase*(i+1));
+                        d.draw(canvas);
                     }
                 }
             }
@@ -136,15 +127,18 @@ public class MazeView extends SurfaceView {
                             canvas.drawRect(y1,x1,y2,x2, paint);
 
                         }
-                        if ( maze.getGrille()[caseX - 2 + i][caseY - 2 + j] == 3){
+                        else if ( maze.getGrille()[caseX - 2 + i][caseY - 2 + j] == 3){
                             paint.setColor(Color.RED);
 
                             canvas.drawRect(y1,x1,y2,x2, paint);
                         }
-                        if ( maze.getGrille()[caseX - 2 + i][caseY - 2 + j] == 0){
+                        else if ( maze.getGrille()[caseX - 2 + i][caseY - 2 + j] == 0){
                             paint.setColor(Color.BLACK);
                             canvas.drawRect(y1,x1,y2,x2, paint);
-
+                        }
+                        else{
+                            d.setBounds(y1,x1,y2,x2);
+                            d.draw(canvas);
                         }
                     }
                     else{
